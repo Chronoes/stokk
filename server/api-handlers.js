8d6351d9c1781a30ce148da90386805dee560b90
= <marten.tarkin@gmail.com>
2015-09-26 23:25:14 +0300

    Registration endpoint added

---
+++ server/api-handlers.js
@@ -0,0 +1,34 @@
+const User = require('./models/User');
+const bcrypt = require('bcrypt-as-promised');
+
+exports.login = (req, res) => {
+  res.send('I am login page');
+};
+
+exports.register = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.sync()
+    .then(() => bcrypt.hash(password, 10))
+    .then(hash => {
+      User
+        .findOrCreate({where: {email: email}, defaults: {email: email, password: hash}})
+        .spread((user, created) => {
+          console.log(user.get({plain: true}));
+          if (created) {
+            res.status(200).json({
+              message: `User created: ID ${user.get('id')}`,
+            });
+          } else {
+            res.status(409).json({
+              message: `User with email ${email} exists`,
+            });
+          }
+        });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: `Hashing failed with ${err}`,
+      });
+    });
+};
---
+++ test/testRegister.js
@@ -0,0 +1,10 @@
+// import {expect} from 'chai';
+// import {routes} from '../server/api-router';
+
+describe('Registration', () => {
+  describe('endpoint', () => {
+    it('should receive a non-empty POST request', () => {
+      // console.log(routes);
+    });
+  });
+});
