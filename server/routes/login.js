7291f2b9dfb74addb1035f3f56892e239b94085a
Chronoes <marten.tarkin@gmail.com>
2015-10-01 15:00:09 +0300

    Added login endpoint

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,3 +1,32 @@
+const User = require('../models/User');
+const util = require('../util');
+
+function doesNotExist(res, email) {
+  res.status(401).json({
+    message: `User ${email} does not exist or password is wrong.`,
+  });
+}
+
 module.exports = (req, res) => {
-  res.status(200).send('TODO: implement login');
+  const email = req.body.email;
+  const password = req.body.password;
+  return User.findOne({where: {email: email}})
+    .then(user => {
+      if (user !== null) {
+        util.compareSaltyHash(password, user.password)
+        .then(() => {
+          res.status(200).json({
+            message: `User authorization successful.`,
+            token: util.signToken({id: user.id, email: email}),
+            id: user.id,
+            email: email,
+          });
+        })
+        .catch(() => {
+          doesNotExist(res, email);
+        });
+      } else {
+        doesNotExist(res, email);
+      }
+    });
 };
