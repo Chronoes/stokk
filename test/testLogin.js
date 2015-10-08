ccaa1b51df19bd8a3f3fe8929229451a082a1763
rain.vink <rain.vink@gmail.com>
2015-10-08 13:37:45 +0300

    Login unittests created

--- test/testLogin.js
+++ test/testLogin.js
@@ -1,21 +1,80 @@
-// import chai, {expect} from 'chai';
-// import supertestChai, {request} from 'supertest-chai';
+import chai, {expect} from 'chai';
+import supertestChai, {request} from 'supertest-chai';
 import app from '../app';
+import jwt from 'jsonwebtoken';
+import fs from 'fs';
 
-// chai.use(supertestChai.httpAsserts);
+
+chai.use(supertestChai.httpAsserts);
 
 
 describe('Login handler', () => {
-  // const route = '/api/login';
+  const route = '/api/login';
 
   let server;
   before(() => {
     server = app.listen(1338);
   });
 
-  it('should return a token for valid user and password');
+  it('should return a token for valid user and password', done => {
+    const secret = fs.readFileSync('./server/secret', 'utf8');
+    const mockRequest = {
+      email: 'test1@stokk.io',
+      password: 'testingpass1',
+    };
 
-  it('should respond with Unauthorized for no user or wrong password');
+    request(server)
+      .post(route)
+      .send(mockRequest)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.have.length.above(0);
+        expect(mockRequest.email).to.have
+          .equals(jwt.verify(res.body.token, secret).email);
+        done();
+      });
+  });
+
+  it('should respond with Unauthorized message (Username wrong)', done => {
+    const mockRequest = {
+      email: 'test11@stokk.io',
+      password: '45tR0nGPa$$w0rD',
+    };
+
+    request(server)
+      .post(route)
+      .send(mockRequest)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        expect(res).to.have.status(401);
+        expect(res.body.message).to.have.length.above(0);
+        done();
+      });
+  });
+
+  it('should respond with Unauthorized message (Password wrong)', done => {
+    const mockRequest = {
+      email: 'test1@stokk.io',
+      password: '4GPa$$w0rD',
+    };
+
+    request(server)
+      .post(route)
+      .send(mockRequest)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        expect(res).to.have.status(401);
+        expect(res.body.message).to.have.length.above(0);
+        done();
+      });
+  });
 
   it('should fail with missing/empty arguments or empty request');
 
