0d1def451d736ee88164207fbd2d1fb2e5d82863
marten.truu <marten.truu@gmail.com>
2015-10-12 20:16:14 +0300

    user dropdown color change and dashboard page for presentation

--- src/scripts/pages/DashboardPage.js
+++ src/scripts/pages/DashboardPage.js
@@ -3,8 +3,15 @@ import React, {Component} from 'react';
 class DashboardPage extends Component {
   render() {
     return (
-      <div>
-        I am the dashboard
+      <div className="dashboard-welcome">
+        <div>WELCOME TO THE DASHBOARD</div>
+        <img className="dashboard-page-image" src="iconlogo.svg" />
+        <p>&quot;Stokking your Stokks is simply amazeballs.&quot; -Bill Gates</p>
+        <p>&quot;tra meie rakendus on nii awesome&quot; -Uku Tammet</p>
+        <p>&quot;I am the best.&quot; -Kanye West</p>
+        <p>&quot;Cool design&quot; -Rain Vink</p>
+        <p>&quot;I couldn&t have designed it better myself.&quot; -Jony Ive</p>
+        <p>&quot;I believed in the holy React!&quot; -Marten Tarkin</p>
       </div>
     );
   }
