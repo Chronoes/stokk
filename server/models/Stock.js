4f2b1e8a1afeb3eeea6ba683ffbc95e703461dc8
Chronoes <marten.tarkin@gmail.com>
2015-10-27 13:07:39 +0200

    Added new database model: Stock

---
+++ server/models/Stock.js
@@ -0,0 +1,46 @@
+import Sequelize from 'sequelize';
+import database from '../database';
+
+export default database.define('stock', {
+  symbol: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isUppercase: true,
+    },
+  },
+
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    notEmpty: true,
+  },
+
+  change: {
+    type: Sequelize.STRING,
+    validate: {
+      is: /^[+-][0-9]+\.[0-9]{2}/,
+    },
+  },
+
+  currentPrice: {
+    type: Sequelize.FLOAT,
+  },
+
+  daysLow: {
+    type: Sequelize.FLOAT,
+  },
+
+  daysHigh: {
+    type: Sequelize.FLOAT,
+  },
+
+  yearLow: {
+    type: Sequelize.FLOAT,
+  },
+
+  yearHigh: {
+    type: Sequelize.FLOAT,
+  },
+});
