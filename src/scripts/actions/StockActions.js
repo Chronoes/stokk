1b9a2fed32e65dfc740f7c13e50306b65046873b
marten.truu <marten.truu@gmail.com>
2015-11-05 10:36:34 +0200

    fixed some miscommunication between backend and frontend.

--- src/scripts/actions/StockActions.js
+++ src/scripts/actions/StockActions.js
@@ -8,7 +8,7 @@ class StockActions {
     const {searchError, searchSuccess} = this.actions;
 
     searchStocks(searchString)
-      .then(response => searchSuccess(response.data.stock))
+      .then(response => searchSuccess(response.data.stocks))
       .catch(response => {
         if (response instanceof Error) {
           searchError(response.data.message);
--- src/scripts/components/StockPreview.js
+++ src/scripts/components/StockPreview.js
@@ -5,7 +5,7 @@ class StockPreview extends Component {
     const {stock} = this.props;
     return (
       <div className="stock-preview">
-        {stock.symbol}&emsp;{stock.name}&emsp;{stock.change}&emsp;
+        {stock[0].symbol}&emsp;{stock[0].name}&emsp;
         <button className="preview-add-button">
           add
         </button>
