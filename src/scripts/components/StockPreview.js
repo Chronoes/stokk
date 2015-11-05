eb3bf1596f918c2d10d83d9636b149bed4567635
Uku Tammet <ukutammet@gmail.com>
2015-11-05 12:49:17 +0200

    use BEM for stock preview

--- src/scripts/components/StockPreview.js
+++ src/scripts/components/StockPreview.js
@@ -4,8 +4,8 @@ const StockPreview = ({stock}) => {
   const {symbol, name} = stock[0];
   return (
     <div className="stock-preview">
-      {`${symbol}  ${name}  `}
-      <button className="preview-add-button">
+      {`${symbol} ${name}`}
+      <button className="stock-preview__add-button">
         add
       </button>
     </div>
