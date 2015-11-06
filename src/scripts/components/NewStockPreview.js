5fdc6863c1bdb9dd721d6e73b58da70ec387f030
marten.truu <marten.truu@gmail.com>
2015-11-07 00:47:51 +0200

    beautified the stock preview beautification solution

--- src/scripts/components/NewStockPreview.js
+++ src/scripts/components/NewStockPreview.js
@@ -15,7 +15,7 @@ class NewStockPreview extends Component {
       <div className="stock-preview">
         <div className="stock-preview__symbol">{`${symbol}`}</div>
         <span className="stock-preview__name">
-          {`${name.length > 40 ? name.substring(0, 40) + '...' : name}`}
+          {`${name}`}
         </span>
         <button
           onClick={this.onAddStockSubmit.bind(this)}
