e9c89bcfca4ce8f6ca06dbd1dae5e4b1d916ff6b
rain.vink <rain.vink@gmail.com>
2015-12-15 02:20:15 +0200

    Extra information table finished

--- src/scripts/components/SingleStockOverview.js
+++ src/scripts/components/SingleStockOverview.js
@@ -2,7 +2,7 @@ import React from 'react';
 import moment from 'moment';
 
 const SingleStockOverview = ({stock}) => {
-  const {change, name, currentPrice, symbol, updatedAt, isPositiveChange} = stock;
+  const {change, name, currentPrice, symbol, updatedAt, isPositiveChange, yearLow, yearHigh} = stock;
   const isPositive = change.charAt(0) === '+';
   const updatedAgo = moment(updatedAt).fromNow();
   const positiveChangeResult = isPositiveChange ?
@@ -11,23 +11,35 @@ const SingleStockOverview = ({stock}) => {
   const changeClassModifier = isPositiveChange ? 'increase' : 'decrease';
   return (
     <div className="dashboard__section">
-      <h4 className="dashboard__heading">
-        {symbol} - {name}
-      </h4>
-      <span className="base-text">
-        {currentPrice} $
-      </span>
-      <span className={`change-label --${isPositive ? 'increase' : 'decrease'}`}>
-        {change}
-      </span>
-      <span className="base-text">
-        {' '} last updated {updatedAgo}
-      </span>
-      <span className={`direction-label --${changeClassModifier}`}>
-        <i className={`direction-label__icon --${changeClassModifier}`} />
-        {positiveChangeResult}
-      </span>
-    </div>
+      <div className="information">
+        <h4 className="dashboard__heading">
+          {symbol} - {name}
+        </h4>
+        <span className=" white base-text">
+          {currentPrice} $
+        </span>
+        <span className={`change-label --${isPositive ? 'increase' : 'decrease'}`}>
+          {change}
+        </span>
+        <span className="base-text">
+          {' '} last updated {updatedAgo}
+        </span>
+        <span className={`direction-label --${changeClassModifier}`}>
+          <i className={`direction-label__icon --${changeClassModifier}`} />
+          {positiveChangeResult}
+        </span>
+        </div>
+        <div className="statistics">
+          <div className="statcard-element">
+            <h3 className="statcard-number white">{yearLow} $</h3>
+            <span className="statcard-desc">Year lowest</span>
+          </div>
+          <div className="statcard-element">
+            <h3 className="statcard-number white">{yearHigh} $</h3>
+            <span className="statcard-desc">Year highest</span>
+          </div>
+        </div>
+      </div>
   );
 };
 
